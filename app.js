d2198a1dd700764588b9841ccbfa0b923c0bda9f
Softas <geosof@a483e73294e8.ant.amazon.com>
2019-09-20 13:09:26 +0100

    Testing v2

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ var app = express();
 
 app.get('/', function(req, res) {
   const response = Object.assign({}, res);
-  
-  res.send({
-    "Output": "Hello Stranger!",
-    "Response": JSON.stringify(response)
-  });
-});
 
-app.get('/:name', function(req, res) {
   if(req.query.name) {
     res.send({
       "Output": `Hello ${req.query.name}`
     })
+  } else {
+    res.send({
+      "Output": "Hello Stranger!",
+      "Response": JSON.stringify(response)
+    });
   }
 });
 
