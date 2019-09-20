e7b894f3901c382feda13595ae47434bc0fd1d66
Softas <geosof@a483e73294e8.ant.amazon.com>
2019-09-20 12:33:11 +0100

    Added get method to test

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/', function(req, res) {
   });
 });
 
+app.get('/name/:name', function(req, res) {
+  if(req.params.name) {
+    res.send({
+      "Output": `Hello ${req.params.name}`
+    })
+  } else {
+    res.send({
+      "Output": "Hello World!"
+    });
+  }
+});
+
 app.post('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
