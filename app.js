bf164a13ee234d05b0bc8caeee064a06fa06477f
Softas <geosof@a483e73294e8.ant.amazon.com>
2019-09-20 12:15:28 +0100

    Initial Deploy Test

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.post('/', function(req, res) {
   });
 });
 
+app.post('/name/:name', function(req, res) {
+  if(req.params.name) {
+    res.send({
+      "Output": `Hello ${req.params.name}`
+    })
+  } else {
+    res.send({
+      "Output": "Hello World!"
+    });
+  }
+});
+
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
